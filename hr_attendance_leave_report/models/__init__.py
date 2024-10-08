738f4dda8625fff49dbb280e3597e90422b89a9f
Alfredo <alfredodelafuente@avanzosc.es>
2024-10-08 13:56:10 +0200

    [16.0][ADD] hr_attendance_leave_report: In Attendances new menu option: "Attendances And Leaves".

---
+++ hr_attendance_leave_report/models/__init__.py
@@ -0,0 +1,6 @@
+from . import hr_attendance_leave
+from . import hr_attendance
+from . import hr_employee
+from . import hr_leave_type
+from . import hr_leave
+from . import resource_calendar_leaves
